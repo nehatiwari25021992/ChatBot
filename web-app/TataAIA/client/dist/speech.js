9ab6e2c4c9ae223eaaebc0236ba3dbacd89cf429
jatin chauhan <jatin.chauhan@shephertz.co.in>
2017-09-25 02:00:33 +0530

    Audio toggle on mic button
    Signed-off-by: jatin chauhan <jatin.chauhan@shephertz.co.in>

--- web-app/TataAIA/client/dist/speech.js
+++ web-app/TataAIA/client/dist/speech.js
@@ -1,6 +1,7 @@
 
 var final_transcript = '';
 var recognizing = false;
+var muteAudio = true;
 var ignore_onend;
 var start_timestamp;
 if (!('webkitSpeechRecognition' in window)) {
@@ -12,28 +13,28 @@ if (!('webkitSpeechRecognition' in window)) {
     recognition.interimResults = true;
 
     recognition.onstart = function() {
-        recognizing = true;
-      //  showInfo('info_speak_now');
-      console.log(baseURL+'TataAIA/client/images/mic-animate.gif')
-        start_img.src = baseURL+'TataAIA/client/images/mic-animate.gif';
+        // recognizing = true;
+        //  showInfo('info_speak_now');
+       
+        start_img.src = baseURL+'TataAIA/client/images/mic-slash.gif';
     };
 
     recognition.onerror = function(event) {
         if (event.error == 'no-speech') {
             start_img.src = baseURL+'TataAIA/client/images/mic.gif';
-           // showInfo('info_no_speech');
+            // showInfo('info_no_speech');
             ignore_onend = true;
         }
         if (event.error == 'audio-capture') {
             start_img.src = baseURL+'TataAIA/client/images/mic.gif';
-         //   showInfo('info_no_microphone');
+            //   showInfo('info_no_microphone');
             ignore_onend = true;
         }
         if (event.error == 'not-allowed') {
             if (event.timeStamp - start_timestamp < 100) {
             //    showInfo('info_blocked');
             } else {
-             //   showInfo('info_denied');
+            //   showInfo('info_denied');
             }
             ignore_onend = true;
         }
@@ -45,12 +46,12 @@ if (!('webkitSpeechRecognition' in window)) {
             return;
         }
         start_img.src = baseURL+'TataAIA/client/images/mic.gif';
-     //   document.getElementById("userInput").innerHTML = "";
+        //   document.getElementById("userInput").innerHTML = "";
         if (!final_transcript) {
             //showInfo('info_start');
             return;
         }
-       // showInfo('');
+        // showInfo('');
         if (window.getSelection) {
             window.getSelection().removeAllRanges();
             var range = document.createRange();
@@ -61,7 +62,7 @@ if (!('webkitSpeechRecognition' in window)) {
     };
 
     recognition.onresult = function(event) {
-      //  console.log(event);
+        //  console.log(event);
         var interim_transcript = '';
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
@@ -73,9 +74,9 @@ if (!('webkitSpeechRecognition' in window)) {
         final_transcript = capitalize(final_transcript);
         document.getElementById("userInput").innerHTML = "";
         userInput.innerHTML = linebreak(final_transcript);
-       // interim_span.innerHTML = linebreak(interim_transcript);
+        // interim_span.innerHTML = linebreak(interim_transcript);
         if (final_transcript || interim_transcript) {
-          //  showButtons('inline-block');
+        //  showButtons('inline-block');
         }
     };
 }
@@ -98,31 +99,36 @@ function startButton(event) {
         recognition.stop();
         return;
     }
+    
+    muteAudio = false
     final_transcript = '';
-    recognition.lang = 6;
-    recognition.start();
+//    recognition.lang = 6;
+//    recognition.start();
     ignore_onend = false;
     userInput.innerHTML = '';
-  //  interim_span.innerHTML = '';
+    //  interim_span.innerHTML = '';
     start_img.src = baseURL+'TataAIA/client/images/mic-slash.gif';
- //   showInfo('info_allow');
-  //  showButtons('none');
+    //   showInfo('info_allow');
+    //  showButtons('none');
     start_timestamp = event.timeStamp;
 }
 
 ///////////////////////////Speach Synthesizer//////////////////////////
 
 function speech_synth(msg){
-    var u = new SpeechSynthesisUtterance();
-    //u.text = document.getElementById("synth_final").value; //'Hello World';
-    u.text = msg //'Hello World';
+    if(muteAudio){
+        var u = new SpeechSynthesisUtterance();
+        //u.text = document.getElementById("synth_final").value; //'Hello World';
+        u.text = msg //'Hello World';
 		
-    u.lang = 'en-US';
-    u.rate = 1.2;
-    u.onend = function(event) {
-       // console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        u.lang = 'en-US';
+        u.rate = 1.2;
+        u.onend = function(event) {
+        // console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        }
+        speechSynthesis.speak(u);
+    // document.getElementById("userInput").innerHTML = "";
     }
-    speechSynthesis.speak(u);
-   // document.getElementById("userInput").innerHTML = "";
+   
 	
 }
